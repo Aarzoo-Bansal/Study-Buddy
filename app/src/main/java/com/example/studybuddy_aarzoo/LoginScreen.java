7eec3216b6747aecd297d9efc397d67a34527056
Aarzoo-Bansal <aarzoo1701@gmail.com>
2024-11-14 23:03:25 -0800

    Added Animation and Welcome Screen

---
+++ app/src/main/java/com/example/studybuddy_aarzoo/LoginScreen.java
@@ -0,0 +1,47 @@
+package com.example.studybuddy_aarzoo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginScreen extends AppCompatActivity {
+
+
+        private static final int SPLASH_DURATION = 2500; // 3 seconds
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_welcome);
+
+            // Hide the action bar if present
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().hide();
+            }
+
+           //Getting all the components from UI and adding animation to it.
+            ImageView logoImageView = findViewById(R.id.logo);
+            TextView appName = findViewById(R.id.appName);
+            TextView subLine = findViewById(R.id.subLine);
+            Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+            logoImageView.startAnimation(fadeIn);
+            appName.startAnimation(fadeIn);
+            subLine.startAnimation(fadeIn);
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(LoginScreen.this, Welcome.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, SPLASH_DURATION);
+
+    }
+}
--- app/src/main/java/com/example/studybuddy_aarzoo/Welcome.java
+++ app/src/main/java/com/example/studybuddy_aarzoo/Welcome.java
@@ -1,36 +1,29 @@
 package com.example.studybuddy_aarzoo;
 
-import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Html;
-import android.text.method.HideReturnsTransformationMethod;
-import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class Welcome extends AppCompatActivity {
 
     private TextInputLayout passwordLayout;
     private TextInputEditText passwordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_welcome_screen);
+        setContentView(R.layout.activity_login_screen);
 
         TextView signupLink = findViewById(R.id.signUpLink);
-        String htmlText = "Don't have an account?  <font color='#0000EE'>Sign up here!</font>";
+        String htmlText = "Don't have an account?  <font color='#58a6ff'>Sign up here!</font>";
         signupLink.setText(Html.fromHtml(htmlText, Html.FROM_HTML_MODE_LEGACY));
         signupLink.setOnClickListener(v -> {
             Log.println(Log.WARN, "Signup link works", "hello from signup link!");
